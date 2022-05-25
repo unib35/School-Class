9bec466b56d33a8b117614bf3ac8ba347443a22d
Jong MIn <71157295+unib35@users.noreply.github.com>
2022-05-25 15:30:57 +0900

    Add files via upload

---
+++ 명품 C++ 프로그래밍/9장/예제/예제 8-6.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class Base{
+	int a;
+	protected:
+		void setA(int a){ this->a = a; }
+	public:
+		void showA() { cout << a; }
+}; 
+
+class Derived : private Base{
+	int b; //setA, showA
+	protected:
+		void setB(int b){ this->b = b; }
+	public:
+		void showB() { 
+			setA(5);
+			showA();
+			cout << b;
+		}
+}; 
+
+class GrandDerived : private Derived{
+	int c; //setB, showB
+	protected:
+		void setAB(int x){
+			setA(x); //컴파일 오류 
+			showA(); 
+			setB(x);
+		}
+}; 
+
+
+
